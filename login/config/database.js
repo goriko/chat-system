f1ccf02b563abedd7612e3b924e34261eb5d0f41
repahsan <xyukiterumix@gmail.com>
2018-03-03 13:48:31 +0800

    login naa na apil sad register na link nasad sa main page pagayo nlng sa design

---
+++ login/config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root'
+    },
+	'database': 'my_schema',
+    'users_table': 'users'
+};
